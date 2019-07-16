f5c4c791be523bb063e62f4fe02524c8c6938be9
atrillanes1972 <45346263+atrillanes1972@users.noreply.github.com>
2019-07-16 18:47:11 +0800

    Create basic test gradle ci
    Create basic test gradle ci, for the basic seed job being tested

---
+++ jobs-for-seed/basic_gradle1.groovy
@@ -0,0 +1,12 @@
+job('test-gradle-ci') {
+    description 'Build and test the app.'
+    scm {
+        github 'sheehan/job-dsl-playground'
+    }
+    steps {
+        gradle 'test'
+    }
+    publishers {
+        archiveJunit 'build/test-results/**/*.xml'
+    }
+}
